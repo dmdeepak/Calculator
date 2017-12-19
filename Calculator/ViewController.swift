013428f18714891c46a8e2676d884e8de64f3912
wonjae0327 <wonjae0327@gmail.com>
2017-12-19 16:31:42 +0900

    ios 계산기 1강 완

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(Double.pi)
+            }
+        }
     }
 }
 
